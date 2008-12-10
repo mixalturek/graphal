7e21fe412826b5fee4302b83ab37be801c082684
Michal Turek <mixalturek@users.sf.net>
2008-12-10 18:44:06 +0000

    Node class added

---
+++ bbd/node.cpp
@@ -0,0 +1,56 @@
+/*
+ *      node.cpp
+ *
+ *      Copyright 2008 Michal Turek <http://woq.nipax.cz/>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+
+#include "node.hpp"
+
+
+/////////////////////////////////////////////////////////////////////////////
+////
+
+Node::Node(void)
+	: BaseObject()
+{
+
+}
+
+
+Node::~Node(void)
+{
+
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+////
+
+void Node::indent(ostream& os, uint indent)
+{
+	for(uint i = 0; i < indent; i++)
+		os << "    ";
+}
+
+
+ostream& operator<<(ostream& os, const Node& node)
+{
+	node.dump(os, 0);
+	return os;
+}
---
+++ bbd/node.hpp
@@ -0,0 +1,52 @@
+/*
+ *      node.hpp
+ *
+ *      Copyright 2008 Michal Turek <http://woq.nipax.cz/>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <memory>
+#include "general.hpp"
+#include "baseobject.hpp"
+#include "context.hpp"
+
+class Value;
+typedef auto_ptr<Value> PTR_Value;
+
+class Node: public BaseObject
+{
+public:
+	Node(void);
+	virtual ~Node(void);
+
+	virtual PTR_Value execute(const Context& context) = 0;
+	virtual void dump(ostream& os, uint indent) const = 0;
+
+	static void indent(ostream& os, uint indent);
+
+private:
+	Node(const Node& object);
+	Node& operator=(const Node& object);
+};
+
+ostream& operator<<(ostream& os, const Node& node);
+
+#endif /* NODE_HPP */
