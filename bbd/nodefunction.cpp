28e05aab69ceb0b3f9943fbb10b67e38b25ee18e
Michal Turek <mixalturek@users.sf.net>
2008-12-27 13:18:53 +0000

    NodeFunction added

---
+++ bbd/nodefunction.cpp
@@ -0,0 +1,89 @@
+/*
+ *      nodefunction.cpp
+ *
+ *      Copyright 2008 Michal Turek <http://woq.nipax.cz/>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+
+#include "nodefunction.hpp"
+#include "nodeemptycommand.hpp"
+#include "valuenull.hpp"
+
+
+/////////////////////////////////////////////////////////////////////////////
+////
+
+NodeFunction::NodeFunction(const list<identifier>& parameters, Node* block)
+	: Node(),
+	m_parameters(parameters),
+	m_block((block != NULL) ? block : new NodeEmptyCommand() )
+{
+
+}
+
+NodeFunction::~NodeFunction(void)
+{
+	delete m_block;
+	m_block = NULL;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+////
+
+RetVal NodeFunction::execute(Context& context)
+{
+	// TODO: catch "return" exception
+	// TODO: built in functions
+
+//	try
+//	{
+		m_block->execute(context);
+/*	}
+	catch(CReturnException& ex)
+	{
+		return ex.GetValue();
+	}
+*/
+	return RetVal(new ValueNull());;
+}
+
+void NodeFunction::dump(ostream& os, uint indent) const
+{
+	dumpIndent(os, indent);
+	os << "<Function>" << endl;
+
+	list<identifier>::const_iterator it;
+
+	for(it = m_parameters.begin(); it != m_parameters.end(); it++)
+	{
+		dumpIndent(os, indent + 1);
+		os << "<Parameter>" << *it << "</Parameter>" << endl;
+	}
+
+	m_block->dump(os, indent + 1);
+
+	dumpIndent(os, indent);
+	os << "</Function>" << endl;
+}
+
+ostream& operator<<(ostream& os, const NodeFunction& node)
+{
+	node.dump(os, 0);
+	return os;
+}
