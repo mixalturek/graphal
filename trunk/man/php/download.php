31eac284ae18b4efa86ac44a26a9d32d47c80807
Michal Turek <mixalturek@users.sf.net>
2009-12-12 12:10:34 +0000

    Download page has been updated

--- trunk/man/php/download.php
+++ trunk/man/php/download.php
@@ -3,20 +3,81 @@ $g_title = 'Download';
 include 'p_begin.php';
 ?>
 
-<p>ChangeLog and TODO files are attached to the archives.</p>
+<p>ChangeLog and TODO files are attached to the archives. Ask for older versions if you want them.</p>
 
-<ul>
-<li><a href="down/bbd_svn269.tar.gz">Tue, 24 Nov 09 12:58:10 +0100, svn v269</a> - Bugfixes, text, presentation (final?)</li>
-<li><a href="down/bbd_svn242.tar.gz">Thu, 05 Nov 09 18:35:48 +0100, svn v242</a> - Bugfixes, text</li>
-<li><a href="down/bbd_svn218.tar.gz">Fri, 25 Sep 09 15:09:54 +0200, svn v218</a> - Final</li>
-<li><a href="down/bbd_svn200.tar.gz">Wed, 09 Sep 09 23:36:27 +0200, svn v200</a> - Visualizations</li>
-<li><a href="down/bbd_svn179.tar.gz">Wed, 01 Jul 09 15:46:58 +0200, svn v179</a> - IDE completed</li>
-<li><a href="down/bbd_svn147.tar.gz">Wed, 13 May 09 21:33:31 +0800, svn v147</a> - GUI</li>
-<li><a href="down/bbd_svn125.tar.gz">Mon, 30 Mar 09 20:55:54 +0800, svn v125</a></li>
-<li><a href="down/bbd_svn119.tar.gz">Sun, 22 Mar 09 21:46:08 +0700, svn v119</a></li>
-<li><a href="down/bbd_svn99.tar.gz">Tue, 24 Feb 09 20:13:30 +0700, svn v99</a></li>
-<li><a href="down/bbd_svn84.tar.gz">Fri, 30 Jan 09 13:31:09 +0100, svn v84</a></li>
-</ul>
+<table>
+<tr>
+<th>Date</th>
+<th>Version</th>
+<th>Note</th>
+</tr>
+
+<tr>
+<td>Sat, 12 Dec 09 12:50:04 +0100</td>
+<td><a href="down/bbd_svn279.tar.gz">svn v279</a></td>
+<td>Final</td>
+</tr>
+
+<tr>
+<td>Tue, 24 Nov 09 12:58:10 +0100</td>
+<td>svn v269</td>
+<td>Bugfixes, text, presentation</td>
+</tr>
+
+<tr>
+<td>Thu, 05 Nov 09 18:35:48 +0100</td>
+<td>svn v242</td>
+<td>Bugfixes, text</td>
+</tr>
+
+<tr>
+<td>Fri, 25 Sep 09 15:09:54 +0200</td>
+<td>svn v218</td>
+<td>Final, application</td>
+</tr>
+
+<tr>
+<td>Wed, 09 Sep 09 23:36:27 +0200</td>
+<td>svn v200</td>
+<td>Visualizations</td>
+</tr>
+
+<tr>
+<td>Wed, 01 Jul 09 15:46:58 +0200</td>
+<td>svn v179</td>
+<td>IDE completed</td>
+</tr>
+
+<tr>
+<td>Wed, 13 May 09 21:33:31 +0800</td>
+<td>svn v147</td>
+<td>GUI</td>
+</tr>
+
+<tr>
+<td>Mon, 30 Mar 09 20:55:54 +0800</td>
+<td>svn v125</td>
+<td></td>
+</tr>
+
+<tr>
+<td>Sun, 22 Mar 09 21:46:08 +0700</td>
+<td>svn v119</td>
+<td></td>
+</tr>
+
+<tr>
+<td>Tue, 24 Feb 09 20:13:30 +0700</td>
+<td>svn v99</td>
+<td></td>
+</tr>
+
+<tr>
+<td>Fri, 30 Jan 09 13:31:09 +0100</td>
+<td>svn v84</td>
+<td></td>
+</tr>
+</table>
 
 <?php
 include 'p_end.php';
