4cfe704f627bc3d4373fc1a61d57696d3f0d2404
Michal Turek <mixalturek@users.sf.net>
2013-04-02 20:45:52 +0200

    Transition to git - Tags directory removed. - Files from trunk moved to the top level directory.

--- tags/print/bbdgui/leaks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-valgrind --leak-check=full --show-reachable=yes ./bbdgui 2>&1 | sed 's/^==.*== //' | sed 's/\(at\|by\) 0x[0-9A-F]*: //' > leaks_`date +%F_%T | tr ':' '-'`.txt
--- tags/print/man/php/p_end.php
+++
@@ -1,7 +0,0 @@
-
-</div><!-- div id="page" -->
-
-<div id="footer">Copyright &copy; 2009 <?php Blank('http://woq.nipax.cz/', 'Michal Turek'); ?></div>
-
-</body>
-</html>
