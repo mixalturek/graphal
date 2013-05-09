8cf8386e1eeef813b87a98ce03ce34b0393e78cb
Michal Turek <mixalturek@users.sf.net>
2013-05-09 18:16:11 +0200

    Web - License header added to all files of web. - Empty aaa.php removed.

--- web/p_begin.php
+++ web/p_begin.php
@@ -1,4 +1,24 @@
 <?php
+/*
+ * Copyright 2013 Michal Turek
+ *
+ * This file is part of Graphal.
+ * http://graphal.sourceforge.net/
+ *
+ * Graphal is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * Graphal is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graphal.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 Header('Content-Type: text/html; charset=utf-8');
 include_once 'p_func.php';
 ?>
