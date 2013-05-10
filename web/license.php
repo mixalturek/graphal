51cf14d40fc372fb9cfa212338a11db2fdadad8c
Michal Turek <mixalturek@users.sf.net>
2013-05-10 18:35:15 +0200

    Web - License page updated.

--- web/license.php
+++ web/license.php
@@ -23,13 +23,66 @@ $g_title = 'License';
 include 'p_begin.php';
 ?>
 
-<p>This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2 of the License.</p>
 
-<p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.</p>
+<h2>Graphal Parts</h2>
 
-<p>You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.</p>
 
-<?php Blank('http://www.gnu.org/licenses/gpl.html'); ?>
+<h3>Graphal Library</h3>
+
+<p>Graphal library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, version 3 of the License.</p>
+
+<p>Graphal library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.</p>
+
+
+<h3>Graphal CLI</h3>
+
+<p>Graphal CLI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.</p>
+
+<p>Graphal CLI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.</p>
+
+
+<h3>Graphal GUI</h3>
+
+
+<p>Graphal GUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3 of the License.</p>
+
+<p>Graphal GUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.</p>
+
+
+<h2>Text of Licenses</h2>
+
+<h3>GNU General Public License</h3>
+
+<pre>
+<?php
+readfile('../COPYING');
+?>
+</pre>
+
+
+<h3>GNU Lesser General Public License</h3>
+
+<pre>
+<?php
+readfile('../COPYING.LESSER');
+?>
+</pre>
+
 <?php
 include 'p_end.php';
 ?>
