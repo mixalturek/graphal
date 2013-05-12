34f40c9773a6f8fbeacafa41f9ccf1c35c019f49
Michal Turek <mixalturek@users.sf.net>
2013-05-12 16:25:47 +0200

    Web - Index page updated.

--- web/index.php
+++ web/index.php
@@ -23,25 +23,37 @@ $g_title = 'Graph Algorithms Interpreter';
 include 'p_begin.php';
 ?>
 
-<p>The core of this diploma thesis is design and implementation of a special language based on C, which will be used for the graph manipulations. Integrated development environment was developed, it consists of a text editor for programmers, debugger and visualization window. The progress of the interpreted graph algorithm can be displayed using 3D graphics.</p>
+<p><em>Graphal project</em> is an interpreter of a programming language mainly oriented to graph algorithms. There is a command line interpreter and a graphical integrated development environtment. The IDE contains a text editor for programmers, compilation and script output, advanced debugger and vizualization window. The progress of the interpreted and debugged graph algorithm can be displayed in 3D scene.</p>
 
 
-<h3>Features</h3>
+<h3>Main Features</h3>
 
-<div id="screenshot"><?php Img('images/screenshots/graphal_gui_dfs_sm.png', 'Screenshot'); ?></div>
+<div id="screenshot"><?php Img('images/screenshots/graphal_gui_dfs_sm.png', 'Screenshot of Graphal GUI'); ?></div>
 
 <ul>
-<li>The code is very similar to C language</li>
-<li>CLI application was written in C++ with STL, clear OOP</li>
-<li>GUI application uses <?php Blank('http://qt.nokia.com/', 'Qt library'); ?></li>
-<li>No other external library dependencies, CLI and GUI code is separated</li>
-<li>Null, bool, int, float, string, array, struct, graph and other data types</li>
-<li>Global and local variables</li>
-<li>C/C++ like evaluation of expressions</li>
-<li>If-else, for, while, foreach statements</li>
-<li>Built-in and user defined functions</li>
-<li>Return, break and continue structured jumps</li>
-<li>Preprocessor includes and defines</li>
+<li>The script code looks very similar to C or Java language.</li>
+<li>Shared library and CLI application is written in C++ with STL.</li>
+<li>GUI application uses Qt library.</li>
+<li>There is no other dependency to any external library.</li>
+<li>CLI and GUI applications are separated.</li>
+</ul>
+
+<ul>
+<li>Null, bool, int, float, string, array, struct, set, vertex, edge, graph and other data types.</li>
+<li>Global and local variables.</li>
+<li>C/C++ like evaluation of expressions.</li>
+<li>If-else, for, while and foreach statements.</li>
+<li>Built-in and user defined functions.</li>
+<li>Return, break and continue structured jumps.</li>
+<li>Preprocessor includes and defines.</li>
+</ul>
+
+<ul>
+<li>File system view.</li>
+<li>Text editor for programmers with syntax highlighting.</li>
+<li>View with compilation and script output.</li>
+<li>Vizualization view with graph in 3D space.</li>
+<li>Advanced debugging of the scripts.</li>
 </ul>
 
 <?php
